61a5d2bfe19f2803d1fb626198f443665acf13b2
samculp <samculp04@yahoo.com>
2025-08-06 22:48:15 -0500

    ..

--- server/db/connection.js
+++ server/db/connection.js
@@ -7,6 +7,7 @@ const uri = process.env.MONGO_URI || ""
 
 async function db() {
   try {
+    console.log("MONGODB URI: ", uri)
     await mongoose.connect(uri, {
       dbName: "expense-tracker"
     })
